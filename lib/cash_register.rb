b61a9e84e821c2e5079786b507b2719a536a8281
Tuong Van Nguyen <tvan262@yahoo.com>
2018-06-01 19:16:28 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,14 @@
 
+require 'pry'
+class CashRegister
+  attr_accessor :total, :discount, :price, :items
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+  
+  
+  
+end
